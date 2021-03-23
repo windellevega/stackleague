a21307387fab0c1f4e65b2fa59e3cfb9f60a6f09
Windelle John Vega <windellevega@gmail.com>
2021-03-23 11:21:44 +0800

    Some comments

--- fraction_decomposition.py
+++ fraction_decomposition.py
@@ -1,10 +1,13 @@
 from fractions import *
 import math
 def decompose(n):
+    # Egyptian Fraction using Fibonacci's algorithm
     if n == '0':
         return []
-    n = n.split('/')
+
     decomposed = []
+
+    n = n.split('/')
 
     if(len(n) == 2):
         numerator = is_numeric(n[0])
@@ -25,9 +28,11 @@ def decompose(n):
 
 
     startDen = 0
-    sumDecompose = Fraction(0)
 
     while fraction.numerator != Fraction(0):
+            # denominator of the largest fraction you can subtract from
+            # a fraction is d = ceil(denominator / numerator)
+            # e.g. 4/5 -> ceil(5/4) = ceil(1.25) = 2 -> 1/2
             startDen = math.ceil(fraction.denominator / fraction.numerator)
             fraction -= Fraction(1,startDen)
             decomposed.append('1/' + str(startDen))
@@ -44,4 +49,5 @@ print(decompose('3/4'))
 print(decompose('13/5'))
 print(decompose('0.19'))
 print(decompose('0.66'))
+print(decompose('2/5'))
 #print((1/2) + (1/7) + (1/59) + (1/5163) + (1/53307975))

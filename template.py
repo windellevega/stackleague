76f14e5827f377d1afbdc30be7579c7cca709e1e
Windelle John Vega <windellevega@gmail.com>
2021-05-14 22:58:34 +0800

    Added template

---
+++ template.py
@@ -0,0 +1,17 @@
+import math
+import itertools
+import re
+import operator
+import collections
+
+def my_func():
+    pass
+
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test___sample(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
